4d8888a2acf2193840c9025a9a0d2d8a75086326
Patrick Michalina <patrickmichalina@mac.com>
2019-01-14 11:42:42 -0600

    fix: npm dist folder pkg root

---
+++ scripts/publish-prep.ts
@@ -0,0 +1,15 @@
+import { copy, ensureDir } from 'fs-extra'
+import { resolve } from 'path'
+
+const targetDir = resolve('dist')
+const srcDir = resolve('src')
+const filesToCopy: ReadonlyArray<string> = [
+  'package.json',
+  'README.md',
+  'LICENSE'
+]
+
+ensureDir(targetDir)
+  .then(() => Promise.all([
+    ...filesToCopy.map(path => copy(path, resolve(targetDir, path)))
+  ]))
