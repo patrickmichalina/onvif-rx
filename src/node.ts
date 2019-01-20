5a410843d531fdc88b8ee17dbfd8d6f4aa9fb03d
Patrick Michalina <patrickmichalina@mac.com>
2019-01-20 15:07:20 -0600

    updates

--- src/node.ts
+++ src/node.ts
@@ -8,3 +8,10 @@ export const createManagedDeviceInNode =
   (config: IDeviceInitConfig) =>
     createManagedDevice(config)
       .run(DEFAULT_NODE_ENV)
+
+
+createManagedDeviceInNode({
+  deviceUrl: 'http://192.168.1.172/onvif/device_service',
+  password: '',
+  username: ''
+}).api.device.getUsers().subscribe(console.log)
