02f6b64556d0aa2f532b756328af2db760580c14
Patrick Michalina <patrickmichalina@mac.com>
2019-01-27 01:04:11 -0600

    wip

--- src/node.ts
+++ src/node.ts
@@ -1,8 +1,5 @@
 import { IDeviceInitConfig, createManagedDevice } from '.'
 import { DEFAULT_NODE_ENV } from './config/node'
-import { maybe } from 'typescript-monads'
-// import { GetProfiles } from './api/media/functions'
-// import { GetAccessPolicy } from './api/device/functions'
 
 export * from './browser'
 export * from './index'
@@ -12,20 +9,17 @@ export const createManagedDeviceInNode =
     createManagedDevice(config)
       .run(DEFAULT_NODE_ENV)
 
-// GetProfiles().run({
-//   system: DEFAULT_NODE_ENV,
-//   deviceUrl: 'http://192.168.1.172/onvif/device_service',
-//   user: maybe({
-//     username: 'admin',
-//     password: 'admin'
-//   })
-// }).subscribe(res => {
-//   res.match({
-//     ok: console.log,
-//     fail: err => console.log(err.status, err.statusMessage)
-//   })
-// })
 
-// createManagedDeviceInNode({
-//   deviceUrl: ''
-// }).api.media
+const device = createManagedDeviceInNode({
+  deviceUrl: 'http://192.168.1.172/onvif/device_service',
+  password: 'admin',
+  username: 'admin'
+})
+
+device.api.ptz.AbsoluteMove()
+  .subscribe((res: any) => {
+    res.match({
+      ok: console.log,
+      fail: (r: any) => console.log(r.status, r.statusMessage)
+    })
+  })
