7133310c56bdcc826f712870752fc68dbacf81d1
Patrick Michalina <patrickmichalina@mac.com>
2019-01-30 02:04:44 -0600

    wip

--- src/node.ts
+++ src/node.ts
@@ -1,6 +1,5 @@
 import { IDeviceInitConfig, createManagedDevice } from '.'
 import { DEFAULT_NODE_ENV } from './config/node'
-import { StreamType, TransportProtocol } from './api/types'
 
 export * from './browser'
 export * from './index'
@@ -9,19 +8,3 @@ export const createManagedDeviceInNode =
   (config: IDeviceInitConfig) =>
     createManagedDevice(config)
       .run(DEFAULT_NODE_ENV)
-
-
-createManagedDeviceInNode({
-  deviceUrl: 'http://192.168.1.172/onvif/device_service',
-  password: 'admin',
-  username: 'admin'
-})
-.api.Media.GetStreamUri({
-  Stream: StreamType['RTP-Unicast'],
-  Transport: {
-    Protocol: TransportProtocol.UDP,
-  }
-}, 'MediaProfile001')
-.subscribe(res => {
-  console.log(res.unwrap().json)
-})
