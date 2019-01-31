957c8f83c2b82dfc05d37ac3530b00a13677efce
Patrick Michalina <patrickmichalina@mac.com>
2019-01-30 23:19:20 -0600

    typings

--- src/node.ts
+++ src/node.ts
@@ -8,19 +8,3 @@ export const createManagedDeviceInNode =
   (config: IDeviceInitConfig) =>
     createManagedDevice(config)
       .run(DEFAULT_NODE_ENV)
-
-
-const device = createManagedDeviceInNode({
-  deviceUrl: 'http://192.168.1.172/onvif/device_service',
-  password: 'admin',
-  username: 'admin'
-})
-
-
-device.api.Media.GetProfiles().toPromise()
-  .then(res => {
-    res.match({
-      fail: r => console.log(r.statusMessage),
-      ok: r => console.log(r.json.Profiles[0])
-    })
-  })
