66fec5925fbfc2306ebaaaac920fc0e93a2bdf37
Patrick Michalina <patrickmichalina@mac.com>
2019-11-18 20:39:20 -0600

    fix

--- src/config/node.ts
+++ src/config/node.ts
@@ -2,43 +2,32 @@ import { DOMParser } from 'xmldom'
 import { ISystemConfig } from './interfaces'
 import { nonce, sharedFetchWrapper } from './universal'
 import { createHash } from 'crypto'
-import { request } from 'http'
+import { RxHR as http } from '@akanass/rx-http-request'
+import { map } from 'rxjs/operators'
 
 const digestSha1 = (str: string) => Promise.resolve(createHash('sha1').update(str).digest('hex'))
 
-const getContent = (url: string, body: string) => {
-  return new Promise((resolve, reject) => {
-    const req = request(url, {
-      method: 'POST',
-      headers: { 
+const getContent =
+  (url: string, body: string) =>
+    http.post(url, {
+      body,
+      headers: {
         'Content-Type': 'application/soap+xml; charset=utf-8;',
         'Content-Length': body.length
       }
-    }, response => {
-      // tslint:disable-next-line: no-if-statement
-      if (!response.statusCode || response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error('Failed to load page, status code: ' + response.statusCode))
-      }
-      // tslint:disable-next-line: readonly-array
-      const body: any[] = []
-      response.on('data', a => body.push(a))
-      response.on('end', () => resolve({
-        text: () => Promise.resolve(body.toString()),
-        status: response.statusCode,
-        statusText: response.statusMessage
-      }))
     })
-    req.on('error', (err) => reject(err))
-    req.write(body)
-    req.end()
-  })
-}
 
 const parser = new DOMParser()
 const transport =
   (body: string) =>
     (uri: string) =>
-      sharedFetchWrapper(getContent(uri, body))
+      sharedFetchWrapper(getContent(uri, body).pipe(map(a => {
+        return {
+          text: () => Promise.resolve(a.body),
+          status: a.response.statusCode,
+          statusText: a.response.statusMessage
+        }
+      })).toPromise())
 
 export const DEFAULT_NODE_ENV: ISystemConfig = {
   parser,
